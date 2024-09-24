6673b94c6a56910b118b8c0b0b918b943c5eb471
Wh1teR88 <wrabb88@gmail.com>
2024-09-24 20:49:30 +0300

    Updated ManualInput
    Added manual input for Barrels and Persons

--- src/ArrayCreation/ManualInput.java
+++ src/ArrayCreation/ManualInput.java
@@ -1,8 +1,9 @@
 package ArrayCreation;
 
 import Entity.Animal;
+import Entity.Barrel;
+import Entity.Person;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -30,7 +31,7 @@ public class ManualInput<T> implements InputManager<T> {
             tempSpecies = scanner.nextLine();
             System.out.println("Введите цвет глаз животного: ");
             tempEyeColor = scanner.nextLine();
-            System.out.println("У животного есть шерсть? 1 - да, любая другая кнопка - нет:");
+            System.out.println("У животного есть шерсть? \"1\" - да, любая другая кнопка - нет:");
             tempWool = scanner.nextLine().equals("1");
             System.out.println("Сколько килограммов весит животное?");
             tempWeight = Double.parseDouble(scanner.nextLine());
@@ -47,10 +48,70 @@ public class ManualInput<T> implements InputManager<T> {
     }
 
     private ArrayList<T> createBarrelArray(int length) {
-        return null;
+        ArrayList<Barrel> barrels = new ArrayList<>();
+        double tempVolume = 0.0;
+        String tempStoredMaterial;
+        String tempMaterial;
+        int added = 0;
+        while (added < length) {
+            boolean volumeIsCorrect = false;
+            while(!volumeIsCorrect) {
+                System.out.println("\nВведите объем бочки в литрах (число больше нуля): ");
+                int userInput = scanner.nextInt();
+                if (userInput > 0) {
+                    volumeIsCorrect = true;
+                    tempVolume = userInput;
+                    scanner.nextLine(); //очистка буфера scanner! не удалять!
+                } else {
+                    System.out.println("Объем бочки не может быть отрицательным, попробуйте снова.");
+                }
+            }
+            System.out.println("Что находится в бочке?");
+            tempStoredMaterial = scanner.nextLine();
+            System.out.println("Введите материал, из которого сделана бочка:");
+            tempMaterial = scanner.nextLine();
+            barrels.add(new Barrel.Builder()
+                    .setVolume(tempVolume)
+                    .setMaterial(tempMaterial)
+                    .setStoredMaterial(tempStoredMaterial)
+                    .build());
+            added++;
+            System.out.println("Добавлено бочек: " + added + ". Осталось добавить: " + (length - added) + ".");
+        }
+        return (ArrayList<T>) barrels;
     }
 
     private ArrayList<T> createPersonArray(int length) {
-        return null;
+        ArrayList<Person> persons = new ArrayList<>();
+        String tempSurname;
+        int tempAge = 0;
+        String tempGender;
+        int added = 0;
+        while (added < length) {
+            System.out.println("\nВведите фамилию человека:");
+            tempSurname = scanner.nextLine();
+            boolean ageIsCorrect = false;
+            while(!ageIsCorrect) {
+                System.out.println("Сколько лет человеку? (введите положительное значение): ");
+                int userInput = scanner.nextInt();
+                if (userInput > 0) {
+                    ageIsCorrect = true;
+                    tempAge = userInput;
+                    scanner.nextLine(); //очистка буфера scanner! не удалять!
+                } else {
+                    System.out.println("Возраст не может быть отрицательным, попробуйте снова.");
+                }
+            }
+            System.out.println("Если человек мужчина - введите \"1\", если женщина - введите люое другое значение");
+            tempGender = scanner.nextLine().equals("1") ? "М" : "Ж";
+            persons.add(new Person.Builder()
+                    .setSurname(tempSurname)
+                    .setAge(tempAge)
+                    .setGender(tempGender)
+                    .build());
+            added++;
+            System.out.println("Добавлено людей: " + added + ". Осталось добавить: " + (length - added) + ".");
+        }
+        return (ArrayList<T>) persons;
     }
 }
