02ec18d72213d6c089cd01cf2e0763d4e67b85e2
Wh1teR88 <wrabb88@gmail.com>
2024-09-26 12:18:32 +0300

    Add Stub-methods to Validate
    -> always true

--- src/Validating/Validate.java
+++ src/Validating/Validate.java
@@ -1,5 +1,9 @@
 package Validating;
 
+import Entity.Animal;
+import Entity.Barrel;
+import Entity.Person;
+
 public class Validate {
     public static boolean validateString(String value) {
         return value != null && !value.trim().isEmpty();
@@ -12,5 +16,11 @@ public class Validate {
     public static boolean validateNonNegativeInt(int value) {
         return value >= 0;
     }
+
+    public static boolean validateBarrel(Barrel barrel) {return true;}
+
+    public static boolean validatePerson(Person person) {return true;}
+
+    public static boolean validateAnimal(Animal animal) {return true;}
 }
 
